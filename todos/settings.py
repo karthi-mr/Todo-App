e1526fb1a5899a326841eea59dd16b35f88ec08f
karthi <mkarthim1998@gmail.com>
2024-03-29 09:57:42 +0530

    requirements and apps added

--- todos/settings.py
+++ todos/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # ? required apps
+    'rest_framework',
+    # ? custom apps
+    'todo_app',
 ]
 
 MIDDLEWARE = [
